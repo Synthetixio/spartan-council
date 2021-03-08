011434c2ce5c4ff97f3042d38c5cdbfedb32b6ae
AndyTCF <andy.c.f.98@gmail.com>
2021-03-08 12:18:14 +1100

    fix up token ticker

--- migrations/1_deploy_spartan_council.js
+++ migrations/1_deploy_spartan_council.js
@@ -2,7 +2,7 @@ const SpartanCouncil = artifacts.require('SpartanCouncil');
 
 module.exports = function(deployer, network, accounts) {
 	const pDAO = accounts[0];
-	deployer.deploy(SpartanCouncil, 'Spartan Council', 'SC', {
+	deployer.deploy(SpartanCouncil, 'Spartan Council', 'SC-NFT', {
 		from: pDAO,
 	});
 };
